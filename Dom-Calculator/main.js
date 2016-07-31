693608016053dc544b5a46200b9c1225bc2b2336
LongHT <thanhlong.vn09@gmail.com>
2016-07-31 22:03:39 +0700

    dom

--- Dom-Calculator/main.js
+++ Dom-Calculator/main.js
@@ -1,7 +1,21 @@
-function getValue(span) {
-    document.getElementById("inputbar").value += span.innerHTML;
+function getValue(x) {
+    document.getElementById("x").value += x.innerText;
 }
 
-function reset(span) {
-    document.getElementById("inputbar").value = ;
+function reset(a) {
+    document.getElementById("x").value = "";
+
+}
+/*tính tổng*/
+function sum() {
+    var x1 = Number(document.getElementById("x").value);
+    document.getElementById("x").value = "";
+
+}
+
+function result() {
+    var x2 = Number(document.getElementById("x").value);
+    // document.getElementById("x").value = x1 + x2;
+    alert(x2);
+    return x1 + x2;
 }
