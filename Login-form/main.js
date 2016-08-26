118dff5c2a36baee7b997d196a97fdfeca948d68
longht1989 <longht@epi.com.vn>
2016-08-26 17:34:43 +0700

    Login form

---
+++ Login-form/main.js
@@ -0,0 +1,13 @@
+var x = decodeURIComponent(document.URL);
+var y = $('#email').val();
+// function myFunction() {
+//     $('#check').html(y);
+// }
+
+
+$(function() {
+    $('#btnsubmit').click(function() {
+        // $('#check').html(y);
+        alert(y);
+    });
+});
