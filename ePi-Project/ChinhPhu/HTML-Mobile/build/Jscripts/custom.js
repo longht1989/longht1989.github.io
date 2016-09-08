29abc4005804fd09edb4298725afada482286395
LongHT <thanhlong.vn09@gmail.com>
2016-09-09 01:17:26 +0700

    done 2 page Kiennghi
    index + category

--- ePi-Project/ChinhPhu/HTML-Mobile/build/Jscripts/custom.js
+++ ePi-Project/ChinhPhu/HTML-Mobile/build/Jscripts/custom.js
@@ -1,23 +1,6 @@
 /* ====================================
    Onload functions
    ==================================== */
-$(".search > span").click(function() {
-    $(".search-box").toggle();
-    $(".search").toggleClass("active");
-})
-
-$(document).ready(function() {
-    $(".epaper-box .slider-listing").bxSlider({
-    	auto: true,
-        pager: false,
-        slideWidth: 110,
-        minSlides: 2,
-        maxSlides: 2,
-        moveSlides: 1,
-        slideMargin: 10,
-        nextText: '',
-        prevText: '',
-        nextSelector: '#epaper-next',
-        prevSelector: '#epaper-prev'
-    });
+$('.main-tab .story').click(function(){
+    $(this).toggleClass('full-info');
 });
