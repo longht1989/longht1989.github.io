68a0bf3a18c74e53e68fa8cb0c409eac4cf3236d
longht1989 <longht@epi.com.vn>
2016-12-01 17:16:47 +0700

    hoàn thành bản desktop trang List.html

--- ePi-Project/Kidy/HTML/build/Jscripts/custom.js
+++ ePi-Project/Kidy/HTML/build/Jscripts/custom.js
@@ -6,6 +6,14 @@ $(function() {
     $('.zone__content.l-grid .item').matchHeight();
     // tooltip for item horizontal
     $('[data-toggle="tooltip"]').tooltip();
+    //markup item--horizontal toggle
+    $('.item--horizontal').each(function(){
+    	$(this).find('.item__markup').click(function(){
+    		$(this).toggleClass('is-marked');
+    		$(this).children('.fa').toggleClass('fa-heart-o');
+    		$(this).children('.fa').toggleClass('fa-heart');
+    	});
+    });
     // jquery ui slider range with fixed minimum 
     $("#filter-distance").slider({
         range: "min",
@@ -14,25 +22,37 @@ $(function() {
         max: 700,
         slide: function(event, ui) {
             $("#amount").val("$" + ui.value);
+        },
+        create: function(event, ui) {
+            var v = $(this).slider('value');
+            // $(this).find('.ui-slider-handle').text(v); //'<span class="under-value">'v'<s/pan>'
+            $(this).find('.ui-slider-handle').append('<span class="under-value"></span>'); //'<span class="under-value">'v'<s/pan>'
+            $(this).find('.ui-slider-handle .under-value').text(v); //'<span class="under-value">'v'<s/pan>'
         }
     });
     // jquery ui slider range 
-    $( "#filter-price" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      values: [ 15, 200 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      }
+    $("#filter-price").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [15, 200],
+        slide: function(event, ui) {
+            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+        },
+        create: function(event, ui) {
+            var v = $(this).slider('value');
+            // $(this).find('.ui-slider-handle').text(v); //'<span class="under-value">'v'<s/pan>'
+            $(this).find('.ui-slider-handle').append('<span class="under-value"></span>'); //'<span class="under-value">'v'<s/pan>'
+            $(this).find('.ui-slider-handle .under-value').text(v); //'<span class="under-value">'v'<s/pan>'
+        }
     });
-    $( "#filter-number" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      values: [ 15, 200 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      }
+    $("#filter-number").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [15, 200],
+        slide: function(event, ui) {
+            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+        }
     });
 });
